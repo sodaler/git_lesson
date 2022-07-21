97b225880911d35d5fe4422d757c09d03bb60975
sodaler <misha_lector87@mail.ru>
2022-07-21 05:17:30 +0400

    resolve conflict

--- app/Http/Controllers/SshController.php
+++ app/Http/Controllers/SshController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SshController extends Controller
 {
-    public function sshFromGitLaravel2()
+    public function sshFromGitLaravel()
     {
         return '';
     }
